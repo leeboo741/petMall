e655c7e7ac7d0fea9ad57984be488a29d2e58650
lee <18811145767@163.com>
2019-10-17 08:37:11 +0800

    201910170836

--- macros/pagePath.js
+++ macros/pagePath.js
@@ -4,7 +4,7 @@ const Page_Home = "/pages/index/index"; // 首页
 const Page_Store_Index = "/pages/store/index"; // 商家 首页
 
 const Page_Mall_Index = "/pages/mall/index"; // 商城 首页
-const Page_Mall_Sstaplefood = "/pages/mall/staplefood/index";  //商城主粮更多
+const Page_Mall_Sstaplefood = "/pages/mall/staplefood/index";  //商城更多
 
 const Page_Me_Index = "/pages/me/index"; // 我的 首页
 
