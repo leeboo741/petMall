1a06eb6e7976487f7cd8c8bc298cb92c025d9f00
Jingbo@li <18811145767@163.com>
2019-10-23 18:28:18 +0800

    201910231828
    1 util
    2 注册页面

--- pages/login/register/index.js
+++ pages/login/register/index.js
@@ -1,11 +1,21 @@
 // pages/login/register/index.js
+
+const intervalDuration = 60;
+const Util = require("../../../utils/util.js");
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    phoneNumber: "", // 输入的电话号码
+    code: null, // 验证码
+    getCodeTitle: "获取验证码", // 获取验证码按钮标题
+    ableGetCode: true, // 是否允许获得验证码
+    intervalID: null, // 获取验证码定时器Id
+    intervalCount: intervalDuration, // 重新获取验证码倒计时
+    cookie: null, // 获取验证码的cookie 包含有sessionId 提交申请的时候 服务器需要 通过sessionId 获取短信验证码 微信每次请求会清空sessionId
   },
 
   /**
@@ -40,7 +50,8 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    clearInterval(this.data.intervalID);
+    this.data.intervalID = null;
   },
 
   /**
@@ -62,5 +73,79 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 输入框输入
+   */
+  inputPhone: function (e) {
+    this.setData({
+      phoneNumber: e.detail.value
+    })
+  },
+
+  /**
+   * 输入验证码
+   */
+  inputCode: function (e) {
+    this.setData({
+      code: e.detail.value
+    })
+  },
+
+  /**
+     * 获取验证码
+     */
+  getCode: function (e) {
+    if (this.data.ableGetCode) {
+      if (Util.checkEmpty(this.data.phoneNumber) || !Util.isPhoneAvailable(this.data.phoneNumber)) {
+        wx.showToast({
+          title: '请输入正确手机号码！',
+          icon: 'none',
+        })
+        return;
+      }
+      console.log("开始倒计时");
+      this.interval();
+      // this.requestCode(this.data.phoneNumber)
+    }
+    console.log("正在倒计时");
+  },
+
+  /**
+   * 开始倒计时
+   */
+  interval: function (e) {
+    let that = this;
+    clearInterval(this.data.intervalID);
+    this.data.intervalID = setInterval(function () {
+      let tempCount = that.data.intervalCount;
+      tempCount--;
+      if (tempCount > 0) {
+        that.setData({
+          getCodeTitle: tempCount + '秒后获取',
+          intervalCount: tempCount,
+          ableGetCode: false
+        })
+        console.log("倒计时===> " + tempCount);
+        that.interval();
+      } else {
+        that.setData({
+          getCodeTitle: "获取验证码",
+          intervalCount: intervalDuration,
+          ableGetCode: true
+        })
+        console.log("倒计时结束");
+        clearInterval(that.data.intervalID);
+        that.data.intervalID = null;
+      }
+    }, 1000);
+  },
+
+  /**
+   * 点击注册
+   */
+  registerAccount: function () {
+    console.log("点击注册");
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -6,7 +6,34 @@ const formatTime = date => {
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+/**
+ * 获取 yyyy-mm-dd 格式时间
+ */
+const formatYMD = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  return [year, month, day].map(formatNumber).join('-')
+}
+
+/**
+ * 获取时间和星期几
+ */
+function dateLater(dates, later) {
+  let dateObj = {};
+  let show_day = new Array('周日', '周一', '周二', '周三', '周四', '周五', '周六');
+  let date = new Date(dates);
+  date.setDate(date.getDate() + later);
+  let day = date.getDay();
+  let yearDate = date.getFullYear();
+  let month = ((date.getMonth() + 1) < 10 ? ("0" + (date.getMonth() + 1)) : date.getMonth() + 1);
+  let dayFormate = (date.getDate() < 10 ? ("0" + date.getDate()) : date.getDate());
+  dateObj.time = yearDate + '-' + month + '-' + dayFormate;
+  dateObj.week = show_day[day];
+  return dateObj;
 }
 
 const formatNumber = n => {
@@ -14,6 +41,42 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+function checkEmpty(obj) {
+  if (obj == null || obj.length <= 0) {
+    return true;
+  }
+  return false;
+}
+
+function isVideo(fileAddress) {
+  let index = fileAddress.lastIndexOf(".");
+  let suffix = fileAddress.substring(index + 1);
+  if (suffix == "mp4" ||
+    suffix == "mov" ||
+    suffix == "m4v" ||
+    suffix == "3gp" ||
+    suffix == "avi" ||
+    suffix == "m3u8" ||
+    suffix == "webm") {
+    return true;
+  }
+  return false;
+}
+
+function isPhoneAvailable(phone) {
+  var myreg = /^[1][3,4,5,6,7,8][0-9]{9}$/;
+  if (!myreg.test(phone)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  dateLater: dateLater,
+  formatYMD: formatYMD,
+  checkEmpty: checkEmpty,
+  isVideo: isVideo,
+  isPhoneAvailable: isPhoneAvailable,
 }
