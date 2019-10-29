32393de3bd8213810e41a287465e7bb31c53f099
lee <18811145767@163.com>
2019-10-29 16:28:51 +0800

    201910291628
    俞增智

--- macros/urlPath.js
+++ macros/urlPath.js
@@ -1,15 +1,15 @@
-const Url_Base = "https://www.baidu.com"; // Base Url
+
 
 /** =================================================== */
 /** 
  *                      网络请求
  */
 /** =================================================== */
-const URL_Service = "http://192.168.3.200:8080"; // 路径
-
+const Url_Base = "http://192.168.3.200:8080"; // Base Url
 
 const Url_ItemType = "/item/type";  //
 
 module.exports={
   Url_Base,  // Base Url
+  Url_ItemType,
 }
